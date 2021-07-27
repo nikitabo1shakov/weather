c34bd890448db6848ae73141e2d0425659f315fe
nikitabolshakov <nikitas.bolshakov@gmail.com>
2021-07-27 18:16:02 +0300

    ViewModel и LiveData

--- app/src/main/java/com/nikitabolshakov/weather/view/MainFragment.kt
+++ app/src/main/java/com/nikitabolshakov/weather/view/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.nikitabolshakov.weather.R
 import com.nikitabolshakov.weather.viewmodel.MainViewModel
 
@@ -27,6 +29,16 @@ class MainFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val observer = Observer<Any> { renderData(it) }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(context, "data", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/nikitabolshakov/weather/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/nikitabolshakov/weather/viewmodel/MainViewModel.kt
@@ -1,7 +1,21 @@
 package com.nikitabolshakov.weather.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val liveDataToObserve: MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
+
+    fun getData(): LiveData<Any> {
+        getDataFromLocalSource()
+        return liveDataToObserve
+    }
+
+    private fun getDataFromLocalSource() {
+        Thread {
+            sleep(2000)
+            liveDataToObserve.postValue(Any())
+        }.start()
+    }
 }
