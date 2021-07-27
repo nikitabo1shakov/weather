772642c0a4ba727da0ce7b53dc4acc1b0e29411e
nikitabolshakov <nikitas.bolshakov@gmail.com>
2021-07-27 19:53:59 +0300

    ViewBinding для активити

--- app/src/main/java/com/nikitabolshakov/weather/view/MainActivity.kt
+++ app/src/main/java/com/nikitabolshakov/weather/view/MainActivity.kt
@@ -3,15 +3,19 @@ package com.nikitabolshakov.weather.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nikitabolshakov.weather.R
+import com.nikitabolshakov.weather.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
         }
     }
