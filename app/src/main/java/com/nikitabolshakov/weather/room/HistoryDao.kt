dad4620491c7c31a67d611a7448fd17732aa75d5
nikitabolshakov <nikitas.bolshakov@gmail.com>
2021-08-21 15:34:57 +0300

    создание content provider

--- app/src/main/java/com/nikitabolshakov/weather/room/HistoryDao.kt
+++ app/src/main/java/com/nikitabolshakov/weather/room/HistoryDao.kt
@@ -1,5 +1,6 @@
 package com.nikitabolshakov.weather.room
 
+import android.database.Cursor
 import androidx.room.*
 
 @Dao
@@ -19,4 +20,13 @@ interface HistoryDao {
 
     @Delete
     fun delete(entity: HistoryEntity)
+
+    @Query("DELETE FROM HistoryEntity WHERE id = :id")
+    fun deleteById(id: Long)
+
+    @Query("SELECT id, city, temperature FROM HistoryEntity")
+    fun getHistoryCursor(): Cursor
+
+    @Query("SELECT id, city, temperature FROM HistoryEntity WHERE id = :id")
+    fun getHistoryCursor(id: Long): Cursor
 }
