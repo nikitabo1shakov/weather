5acd1e5ec4b9f777063a7f2a1ce3df36acefe9d6
nikitabolshakov <nikitas.bolshakov@gmail.com>
2021-10-10 14:45:08 +0300

    добавление Interceptor

--- app/src/main/java/com/nikitabolshakov/weather/data/repository/remote/RemoteDataSource.kt
+++ app/src/main/java/com/nikitabolshakov/weather/data/repository/remote/RemoteDataSource.kt
@@ -3,11 +3,23 @@ package com.nikitabolshakov.weather.data.repository.remote
 import com.google.gson.GsonBuilder
 import com.nikitabolshakov.weather.BuildConfig
 import com.nikitabolshakov.weather.data.model.response.WeatherDTO
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 class RemoteDataSource {
+
+    inner class RemoteDataSourceInterceptor : Interceptor {
+
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            return chain.proceed(chain.request())
+        }
+    }
 
     private val weatherAPI = Retrofit.Builder()
         .baseUrl("https://api.weather.yandex.ru/")
@@ -15,9 +27,18 @@ class RemoteDataSource {
             GsonConverterFactory.create(
                 GsonBuilder().setLenient().create()
             )
-        ).build().create(WeatherAPI::class.java)
+        )
+        .client(createOkHttpClient(RemoteDataSourceInterceptor()))
+        .build().create(WeatherAPI::class.java)
 
     fun getWeatherDetails(lat: Double, lon: Double, callback: Callback<WeatherDTO>) {
         weatherAPI.getWeather(BuildConfig.WEATHER_API_KEY, lat, lon).enqueue(callback)
+    }
+
+    private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor)
+        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        return httpClient.build()
     }
 }
